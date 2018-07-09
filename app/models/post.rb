64eb04efcd9abf2103fc4d97d120774b835fe391
Lucy Wall <lhwall@gmail.com>
2018-07-09 20:28:11 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  #validate :clickbait
+
+  def clickbait
+    self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Guess") || self.title.include?("Top #{/\d+/}")
+  end
+
 end
